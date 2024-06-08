91321b5449cfb427d9c45d656e792c2e41f65fde
Svitlana Pushchalo <svitlana8@gmail.com>
2024-06-09 00:06:21 +0300

    "Python Framework"

--- lib/assertions.py
+++ lib/assertions.py
@@ -18,12 +18,12 @@ class Assertions:
     # ф-ія не повертатиме значення, а порівнюватиме значення з очікуваним значенням
 
     @staticmethod
-    def assert_json_value_by_name(response: Response, name, expected_value, error_message):
+    def assert_json_value_by_name(response: Response, key_name, expected_value, error_message):
         assert response.status_code == 200, 'Wrong status code'
         try:
             response_as_dict = response.json()
         except JSONDecodeError:
             assert False, f"Response is not in json format. Response text is {response.text}"
 
-        assert name in response_as_dict, f"Response JSON doesn't have key {name}"
-        assert response_as_dict[name] == expected_value, error_message
+        assert key_name in response_as_dict, f"Response JSON doesn't have key {key_name}"
+        assert response_as_dict[key_name] == expected_value, error_message
